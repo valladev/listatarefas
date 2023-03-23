53896f6e3c662205e379a33f61af08900a2cc63c
João L. Barboza <105928845+valladev@users.noreply.github.com>
2023-03-23 14:46:43 -0300

    completeFunctionAddAndDelete

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
+
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+
+import "./App.css";
+
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: 'Estudar programação',
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "ler livros",
+      completed: true,
+    },
+  ]);
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return{ ...task, completed: !task.completed}
+      
+      return task;
+    });
+    setTasks(newTasks); 
+  }
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [
+      ...tasks, 
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed:false,
+      },
+    ];
+
+    setTasks(newTasks);
+  };
+
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task =>task.id !== taskId);
+
+    setTasks(newTasks)
+  };
+
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <AddTask handleTaskAddition={handleTaskAddition}/>
+        <Tasks 
+        tasks={tasks} 
+        handleTaskClick={handleTaskClick}
+        handleTaskDeletion={handleTaskDeletion}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const Header = () => {
+    return ( <h1 style={{ color: "#eee" }}>Minha tarefas</h1> );
+}
+ 
+export default Header;
